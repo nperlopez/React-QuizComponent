17cb388be61d29a65c5b534b4b7be4e8bcb3c9ad
Noemi <34232738+nperlopez@users.noreply.github.com>
2019-02-18 10:00:41 +0100

    Updated module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -23,9 +23,8 @@ class Quiz extends Component {
       <div>
         { isQuizEnd() ?
           <QuizEnd/> :
-          ''
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
         }
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
       </div>
     );
   };
